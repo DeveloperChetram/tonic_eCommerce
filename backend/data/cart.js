2518dee3d9711a3f507c39741574022e2be9be7c
developerchetram <patelchetram49@gmail.com>
2025-11-28 12:54:26 -0800

    final draft of ui and backedn

--- backend/data/cart.js
+++ backend/data/cart.js
@@ -1,19 +1,12 @@
 export let cart = [];
 
-let lastClearTime = Date.now();
-
 export const addToCart = (product) => {
-    const now = Date.now();
-    // 24 hours = 24 * 60 * 60 * 1000 milliseconds = 86400000 ms
-    if (now - lastClearTime > 86400000) {
-        cart.length = 0; // Clear cart
-        lastClearTime = now;
-    }
-
     let existing = cart.find(item => item.id === product.id);
+    // console.log('existing', existing)
 
     if (existing) {
         existing.quantity += product.quantity;
+        // console.log('cart', cart)
     } else {
         cart.push(product);
     }
